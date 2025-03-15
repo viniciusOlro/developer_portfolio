5313ba0acc3cc0c15df4947b8b3421538c728a76
Vinicius de Oliveira <ti.vinicius.dev@gmail.com>
2025-03-15 19:52:49 -0300

    Animate aside menu

--- components/AsideMenu.vue
+++ components/AsideMenu.vue
@@ -1,10 +1,14 @@
 <template>
-  <aside>
+  <aside :class="{ 'is-expanded': isExpanded }">
     <header>
-      <RiArrowDownSFill />
-      <h2>{{ title }}</h2>
+      <RiArrowDownSFill :class="['arrow', { 'is-expanded': isExpanded }]" @click="isExpanded = !isExpanded"/>
+      <transition name="fade" mode="out-in">
+        <h2 v-if="isExpanded">{{ title }}</h2>
+      </transition>
     </header>
-    <slot name="content" />
+    <transition name="fade" mode="out-in">
+      <slot v-if="isExpanded" name="content" />
+    </transition>
   </aside>
 </template>
 
@@ -20,30 +24,55 @@ export default {
       type: String,
       required: true
     }
+  },
+  data() {
+    return {
+      isExpanded: true
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 aside {
-  width: 17.45%;
-  min-width: 20rem;
+  width: 5rem;
   max-width: 30rem;
   border-right: .1rem solid var(--secondary-color);
   overflow-y: auto;
   overflow-x: hidden;
+  transition: ease-in-out .7s all;
+  
+  &.is-expanded {
+    width: 17.45%;
+
+    header {
+      justify-content: flex-start;
+    }
+  }
 
   header {
     display: flex;
     align-items: center;
+    justify-content: center;
+    height: 5rem;
     gap: 1.2rem;
-    padding: 1.2rem 2.4rem;
+    padding: 0 2.4rem;
     color: var(--text-light-color);
     border-bottom: .1rem solid var(--secondary-color);
 
+    .arrow {
+      transform: rotate(-90deg);
+      transition: transform .5s ease-in-out;
+      &.is-expanded {
+        transform: rotate(0deg);
+      }
+    }
+
     svg {
-      width: 1.6rem;
-      height: 1.6rem;
+      width: 2rem;
+      min-width: 2rem;
+      height: 2rem;
+      min-height: 2rem;
       cursor: pointer;
     }
 
